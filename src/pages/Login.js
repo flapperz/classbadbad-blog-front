b7703ab26256ab417ab2e37085b5773fbef5aaad
Jinwara <naon123@hotmail.com>
2020-11-07 21:26:01 +0700

    create Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import { makeStyles } from "@material-ui/core/styles";
+import InputBase from '@material-ui/core/InputBase';
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import Button from '@material-ui/core/Button';
+
+const Login = ({ history }) => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  return (
+    <div className={classes.container}>
+      <h1 style={{ marginBottom: 60 }}>Welcome,</h1>
+      <div className={classes.textFieldContainer}>
+        <InputBase
+          className={classes.textField}
+          placeholder="Username"
+        />
+      </div>
+      <div className={classes.textFieldContainer}>
+        <InputBase
+          className={classes.textField}
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                style={{ width:30, height:30 }}
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              >
+                {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </div>
+      <Button
+        className={classes.button}
+        onClick={() => {
+          history.push("/home")
+        }}
+      >
+        Log in
+      </Button>
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh"
+  },
+  textFieldContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    width: 348,
+    backgroundColor: "#E8EBE6",
+    borderRadius: 4,
+    marginBottom: 12
+  },
+  textField: {
+    height: 30,
+    width: 320
+  },
+  button: {
+    height: 36,
+    width: 120,
+    color: "white",
+    background: "linear-gradient( #9399DB 30%, #9399DB 70%)",
+    marginTop: 20
+  }
+});
+
+export default Login;
