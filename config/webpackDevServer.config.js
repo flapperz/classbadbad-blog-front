68b793826b048f269c5b8d77a6d323f580361fec
Krit Cholapand <flap.atyz@gmail.com>
2020-11-27 11:52:50 +0700

    CSP unsafe inline

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -42,7 +42,7 @@ module.exports = function (proxy, allowedHost) {
     headers: {
       "X-Frame-Options": "sameorigin",
       "Content-Security-Policy":
-        "default-src 'none'; script-src 'self'; connect-src 'self' https://classbadbad-back.herokuapp.com/; img-src 'self' *; style-src 'self' *;base-uri 'self';form-action 'self'",
+        "default-src 'none'; script-src 'self'; connect-src 'self' https://classbadbad-back.herokuapp.com/; img-src 'self' *; style-src 'self' 'unsafe-inline';base-uri 'self';form-action 'self'",
       "Cache-Control": "no-cache, no-store, must-revalidate",
       Expires: 0,
       Pragma: "no-cache",
